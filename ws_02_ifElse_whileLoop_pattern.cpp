3cdf21bdc4f535b2e207981c827fd49681176870
AbhishekSharma-Tech <abhisheksharma6923@gmail.com>
2022-07-28 11:05:44 +0530

    First Day of CPP DSA

--- ws_02_ifElse_whileLoop_pattern.cpp
+++ ws_02_ifElse_whileLoop_pattern.cpp
@@ -35,17 +35,17 @@ int main() {
 //	
 //---------------HomeWork---------------
 //	
-//	char ch;
-//	cin>>ch;
-//	
-//	if(ch>='a' && ch<='z')
-//		cout<<"Lower Case"<<endl;
-//	else if(ch>='A' && ch<='Z')
-//		cout<<"Upper Case"<<endl;
-//	else if(ch>='0' && ch<='9')
-//		cout<<"Numeric"<<endl;
-//	else
-//		cout<<"Special Char"<<endl;
+	char ch;
+	cin>>ch;
+	
+	if(ch>='a' && ch<='z')
+		cout<<"Lower Case"<<endl;
+	else if(ch>='A' && ch<='Z')
+		cout<<"Upper Case"<<endl;
+	else if(ch>='0' && ch<='9')
+		cout<<"Numeric"<<endl;
+	else
+		cout<<"Special Char"<<endl;
 
 
 //============== While Loop ===============
