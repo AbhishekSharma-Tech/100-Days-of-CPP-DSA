8eb1b9cccc702e19c6f981abd6621c9d88a55bd4
AbhishekSharma-Tech <abhisheksharma6923@gmail.com>
2022-08-16 22:31:18 +0530

    Day-21

---
+++ ws_35_recursionQuickSort.cpp
@@ -0,0 +1,68 @@
+// S.C -> O(n)
+// T.C -> O(n logn)
+// Worst Case T.C -> O(n^2)
+
+
+
+#include <iostream>
+using namespace std;
+
+int partition (int arr[], int s, int e) {
+
+    int pivot = arr[s];
+
+    int cnt = 0;
+    for(int i=s+1; i<=e; i++)  {
+        if(arr[i] <= pivot)
+            cnt++;
+    }
+
+    // place pivot at right position
+    int pivotIndex = s + cnt;
+    swap(arr[pivotIndex], arr[s]);
+
+    // left & right part ke liye
+    int i = s, j = e;
+
+    while(i < pivotIndex && j > pivotIndex) {
+        while(arr[i] <= pivot){
+            i++;
+        }
+        while(arr[j] > pivot){
+            j--;
+        }
+
+        if(i < pivotIndex && j > pivotIndex) {
+            swap(arr[i++], arr[j--]);
+        }
+    }
+    return pivotIndex;
+}
+
+void quickSort (int arr[], int s, int e) {
+
+    //base case
+    if (s >= e)
+        return ;
+
+    // partition 
+    int p = partition(arr, s, e);
+
+    // sort left part
+    quickSort(arr, s, p-1);
+
+    // right part sort
+    quickSort(arr, p+1, e);
+}
+
+int main() {
+
+    int arr[5] = {2, 4, 1, 6, 9};
+    int n = 5;
+
+    quickSort(arr, 0, n-1);
+
+    for(int i=0; i<n; i++)
+        cout<<arr[i]<<" ";
+
+}
