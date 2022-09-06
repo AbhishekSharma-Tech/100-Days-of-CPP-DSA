9f2cec3f27515494a1b1e541b6aef0bb8fccb3fb
AbhishekSharma-Tech <abhisheksharma6923@gmail.com>
2022-09-06 22:42:07 +0530

    Day-42

---
+++ ws_58_designSpecialStackProblem.cpp
@@ -0,0 +1,69 @@
+//----- Q. Design a stack that supports getMin() in O(1) time and O(1) extra space -----
+
+// #include <stack>
+// // #include <limits.h>
+// class SpecialStack {
+//     // Define the data members.
+//     stack<int> s;
+//     int mini;               // int mini = INT_MAX;
+//     /*----------------- Public Functions of SpecialStack -----------------*/
+//     public:
+        
+//     void push(int data) {
+//         // for 1st element
+//         if(s.empty()) {
+//             s.push(data);
+//             mini = data;
+//         }
+//         else {
+//             if(data < mini) {
+//                 s.push(2*data - mini);
+//                 mini = data;
+//             }
+//             else {
+//                 s.push(data);
+//             }
+//         }
+//     }
+
+//     int pop() {
+//         if(s.empty()) {
+//             return -1;
+//         }
+//         int curr = s.top();
+//         s.pop();
+//         if(curr > mini) {
+//             return curr;
+//         }
+//         else {
+//             int prevMin = mini;
+//             int val = 2*mini - curr;
+//             mini = val;
+//             return prevMin;
+//         }
+//     }
+
+//     int top() {
+//         if(s.empty()) {
+//             return -1;
+//         }
+//         int curr = s.top();
+//         if(curr < mini) {
+//             return mini;
+//         }
+//         else {
+//             return curr;
+//         }
+//     }
+
+//     bool isEmpty() {
+//         return s.empty();
+//     }
+
+//     int getMin() {
+//         if(s.empty()) {
+//             return -1;
+//         }
+//         return mini;
+//     }  
+// };
